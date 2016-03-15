2bde44bdabb911b14dca9a5b11f07eedde707081
zzcgit <zzcforever2010@163.com>
2016-03-15 15:34:10 +0800

    second fix
    fix the wrong code

--- Test.php
+++ Test.php
@@ -4,7 +4,7 @@ class Test extends PHPUnit_Framework_TestCase
 {
 	public function testOnePlusOne() {
 		$this->assertEquals(1+1,2);
-		$this->assertEquals(2+1,1);
+		$this->assertEquals(2+1,3);
   	}
 }
 
