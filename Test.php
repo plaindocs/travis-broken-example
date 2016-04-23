2c20bad477cde76b2c913bb028e6747f201c712e
leonardosouza <leonardo.souza@vivareal.com>
2016-04-23 01:41:41 -0300

    adding test

--- Test.php
+++ Test.php
@@ -5,6 +5,10 @@ class Test extends PHPUnit_Framework_TestCase
 	public function testOnePlusOne() {
 		$this->assertEquals(1+1,2);
 	}
+
+  public function testTwoTimesTwo() {
+    $this->assertEquals(2*2,5);
+  }  
 }
 
 ?>
