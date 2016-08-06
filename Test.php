8d09e2e9a9ff0e934bf631ff293e9a320fd26a30
son69 <lnts1309@gmail.com>
2016-08-06 19:15:30 +0700

    add new test

--- Test.php
+++ Test.php
@@ -5,6 +5,9 @@ class Test extends PHPUnit_Framework_TestCase
 	public function testOnePlusOne() {
 		$this->assertEquals(1+1,2);
   	}
+    public function testOnePlusTwo() {
+        $this->assertEquals(1+2,3);
+    }
 }
 
 ?>
