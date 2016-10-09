9640465dae39849120928a906a5fe6cb0f7a4fb7
Miyuru Sankalpa <miyurusankalpa@users.noreply.github.com>
2016-10-10 00:50:55 +0530

    Update Test.php

--- Test.php
+++ Test.php
@@ -8,6 +8,12 @@ class Test1 extends PHPunit_Framework_Testcase {
 
     public function testOnePlusOne(){
         //include(__FILE__);
+	    $dir = new DirectoryIterator(dirname(__FILE__));
+foreach ($dir as $fileinfo) {
+    if (!$fileinfo->isDot()) {
+        var_dump($fileinfo->getFilename());
+    }
+}
         
 	$result = $lang['BEEIMG'];
         $this->assertEquals("BeeIMG", $result);
