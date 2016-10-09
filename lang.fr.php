8d1e235d756763a0925e78b231167f220036eab0
Miyuru Sankalpa <miyurusankalpa@users.noreply.github.com>
2016-10-10 00:38:04 +0530

    Add files via upload

---
+++ lang.fr.php
@@ -0,0 +1,60 @@
+<?php
+
+/*------------------
+Language: French
+------------------*/
+
+$lang = array();
+
+$lang['BEEIMG'] = 'BeeIMG';
+
+// Titles
+$lang['TITLE_HOME'] = 'Service gratuit d\'hébergment et de partage d\'images';
+$lang['TITLE_UPLOAD'] = 'Téléverser des images';
+$lang['TITLE_RECENT'] = 'Images récentes';
+$lang['TITLE_STATS'] = 'Statistiques';
+$lang['TITLE_TOP'] = 'Top 9 des images';
+$lang['TITLE_ABOUT'] = 'À propos';
+$lang['TITLE_CONTACT'] = 'Contact';
+$lang['TITLE_TOS'] = 'Conditions d\'utilisation';
+$lang['TITLE_REGISTER'] = 'S\'enregistrer';
+$lang['TITLE_LOGIN'] = 'Se connecter';
+$lang['TITLE_DASHBOARD'] = 'Tableau de bord';
+
+// Titles
+$lang['MENU_HOME'] = 'Acceuil';
+$lang['MENU_HOME_TITLE'] = 'Notre acceuil';
+$lang['MENU_UPLOAD'] = 'Téléverser';
+$lang['MENU_UPLOAD_TITLE'] = 'Téléverser des images';
+$lang['MENU_RECENT'] = 'Images récentes';
+$lang['MENU_RECENT_TITLE'] = 'Images récement téléversées';
+$lang['MENU_STATS'] = 'Statistiques';
+$lang['MENU_STATS_TITLE'] = 'Voir les statistiques';
+$lang['MENU_TOP'] = 'Top 9';
+$lang['MENU_TOP_TITLE'] = 'Voir le top 9 des images';
+$lang['MENU_FOLLOW'] = 'Suivre';
+
+// Upload Carousel
+$lang['UPLOAD_CAR_LP'] = 'Acceuil';
+$lang['UPLOAD_CAR_1'] = 'Aucun IP ne sera enregistré durant le téléversement.';
+$lang['UPLOAD_CAR_2'] = 'Nous modérons vos téléversements.';
+$lang['UPLOAD_CAR_3'] = 'La publicité n\'est pas acceptée.';
+
+// Upload Carousel
+$lang['FOOTER_CONTACT'] = 'contact';
+$lang['FOOTER_API'] = 'api';
+$lang['FOOTER_BLOG'] = 'blog';
+$lang['FOOTER_TOS'] = 'cgu';
+$lang['FOOTER_ABOUT_US'] = 'à propos';
+
+// Stats
+$lang['STATS_HEAD'] = 'Statistiques';
+
+// Image View Page
+$lang['IMG_VP_IP_HEAD'] = 'Aperçu de l\'image';
+$lang['IMG_VP_SHARE_HEAD'] = 'Partager cette image';
+$lang['IMG_VP_SHARE_DL'] = 'Télécharger';
+$lang['IMG_VP_SHARE_DL_BTN_IMG'] = 'Image';
+$lang['IMG_VP_SHARE_DL_BTN_ZIP'] = 'Zip';
+$lang['IMG_VP_SHARE_BTNS_HEAD'] = 'Boutons de partage';
+?>
