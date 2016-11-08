e7f163ab2c492c08a3fe0dec9de5267a5fbe58ee
ranjeetkk <ranjeetkk@users.noreply.github.com>
2016-11-08 20:46:18 +0530

    Update Test.php

--- Test.php
+++ Test.php
@@ -3,7 +3,11 @@
 class Test extends PHPUnit_Framework_TestCase
 {
 	public function testOnePlusOne() {
-		$this->assertEquals(1+1,1);
+		$this->assertEquals(1+1+2,4);
+		$this->assertEquals(2+1+1,4);
+		$this->assertEquals(5-2,3);
+		$this->assertEquals(6/2,3);
+		$this->assertEquals(2*3,6);
   	}
 }
 
