d328a4e6cd44dccb8de9daee3f4eb8335eda4d47
D. Zakrzewski <dominiol@o2.pl>
2017-05-02 12:36:04 +0200

    added new test cases

--- Test.php
+++ Test.php
@@ -5,6 +5,14 @@ class Test extends PHPUnit_Framework_TestCase
 	public function testOnePlusOne() {
 		$this->assertEquals(1+1,2);
   	}
+
+	public function testTwoPlusTwo() {
+		$this->assertEquals(2+2,3);
+  	}
+
+	public function testThreePlusThree() {
+		$this->assertEquals(3+3,6);
+  	}
 }
 
 ?>
